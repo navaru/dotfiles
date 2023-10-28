fb201133a4dff8ac2384ffd4f711f86d28eb0830
Eugen Tudorancea <eugen.tudorancea@gmail.com>
2023-10-28 22:17:29 +0300

    refactor: improved prompt and completions

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,43 +1,13 @@
 # zsh path plugins, extensions, etc
-fpath=($HOME/.zsh $fpath)
+fpath=(
+  $HOME/.zsh
+  "${fpath[@]}"
+)
 
 # history
-HISTFILE=$HOME/.zsh_history
-HISTSIZE=1000
-SAVEHIST=1000
-
-
-# ls colors
-autoload -Uz colors; colors;
-
-setopt No_Beep
-setopt auto_cd
-setopt multios
-setopt prompt_subst  # setup the prompt with pretty colors
-
-setopt append_history
-setopt extended_history
-setopt hist_expire_dups_first
-setopt hist_ignore_dups # ignore duplication command history list
-setopt hist_ignore_space
-setopt hist_verify
-setopt inc_append_history
-setopt share_history # share command history data
-
-
-# Completion
-autoload -Uz compinit; compinit -u;
-
-zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-zstyle ':completion:*' list-colors ''
-zstyle ':completion:*' expand 'yes'
-zstyle ':completion:*:*:*:default' menu yes select
-zstyle ':completion:*:*:default' force-list always
-zstyle ':completion:*:match:*' original only
-
-zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
-zstyle ':completion:*:*:kill:*' menu yes select
-
+HISTFILE=$HOME/.history.zsh
+HISTSIZE=5000
+SAVEHIST=5000
 
 # Show history
 case $HIST_STAMPS in
@@ -46,3 +16,13 @@ case $HIST_STAMPS in
   "yyyy-mm-dd") alias history='fc -il 1' ;;
   *) alias history='fc -l 1' ;;
 esac
+
+# ls colors
+autoload -Uz colors; colors;
+
+setopt No_Beep
+setopt auto_cd
+setopt multios
+
+# completion
+autoload -Uz compinit; compinit -u;
